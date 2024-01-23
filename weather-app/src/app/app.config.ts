4a53bfacafcd2dd2feccfdbb10b75e67a9e9a5f7
Redtius <redtius@gmail.com>
2024-01-24 00:06:29 +0100

    added pwa

--- weather-app/src/app/app.config.ts
+++ weather-app/src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideAnimations(),
-    importProvidersFrom(HttpClientModule),
-  ]
+    importProvidersFrom(HttpClientModule), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
